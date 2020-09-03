da10ca8462bc39125832da71b553df2c798e21b6
cjjland76 <cjjland76@yahoo.co.jp>
2020-09-03 18:39:54 +0900

    card.js編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   get 'users/show'
   get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   root 'items#index'
 
   resources :users
+  resources :cards
 end
