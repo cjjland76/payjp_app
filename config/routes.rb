06ed442569c961792facea7ab3847ac7440006e5
cjjland76 <cjjland76@yahoo.co.jp>
2020-09-03 16:33:33 +0900

    マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root 'items#index'
+
+  resources :users
 end
